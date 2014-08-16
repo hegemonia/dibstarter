bf5a55763a85ad30801446eaf737b9f18c69450c
Andrew Kiellor <akiellor@gmail.com>
2014-08-16 12:10:07 -0700

    Add default user for test.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 
+unless Rails.env.production?
+  User.create!({:email => "test@dibstarter.com", :password => "password", :password_confirmation => "password" })
+end
+
 products = Product.create([{ name: 'Design One', design_id: '1', preview_url: '/images/coupon.jpg'},
 	{ name: 'Design Two', design_id: '2', preview_url: '/images/coupon.jpg'}])

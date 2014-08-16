183059be167b5a6812479139d6772bcb19ced548
emccallum <mccallum.ek@gmail.com>
2014-08-15 23:22:13 -0700

    Adding bootstrap js, user login for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
   # GET /products.json
   def index
     @products = Product.all
+    @user = current_user
   end
 
   # GET /products/1

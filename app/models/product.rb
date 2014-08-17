02fffb416afa27b336644639c0df3eb045d2e204
Andrew Kiellor <akiellor@gmail.com>
2014-08-16 21:42:50 -0700

    Basic dibs progress bar.

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,8 +5,28 @@ class Product < ActiveRecord::Base
 
   validates_presence_of :price
 
+  def dibs
+    Dib.initial.where(product_id: self.id)
+  end
+
+  def conversion_percentage
+    [(converted.to_f / threshold.to_f * 100), 100].min
+  end
+
+  def converted
+    dibs.count
+  end
+
+  def threshold
+    3
+  end
+
+  def outstanding
+    [threshold - dibs.count, 0].max
+  end
+
   def dibbed? user
-    user && Dib.where(user_id: user.id, product_id: self.id).first.present?
+    user && dibs.where(user_id: user.id).first.present?
   end
 
   def description

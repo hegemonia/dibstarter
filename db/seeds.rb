d7a41b26fc9435200e9d23d41325ae1def05e0fa
Andrew Kiellor <akiellor@gmail.com>
2014-08-16 22:16:51 -0700

    Removed seed designs.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,3 @@
 unless Rails.env.production?
   User.create!({:email => "test@dibstarter.com", :password => "password", :password_confirmation => "password" })
 end
-
-products = Product.create([{ name: 'Design One', design_id: '1', design_type: 'dtg', preview_url: '/images/EliseSari.png', price: '20.00'},
-	{ name: 'Design Two', design_id: '2', design_type: 'dtg', preview_url: '/images/EliseSari.png', price: '15.00'}])
